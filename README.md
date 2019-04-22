5af58c5f7c6e3dbd47057d9adfd1eb1fb9bfd215
kjb4494 <rlagnltn1357@gmail.com>
2019-04-22 18:19:08 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# docker-hello-django
+## Docker Image Test Project
+
+#### 메모장
+- [*참고 사이트*](https://whatisthenext.tistory.com/124)
+- 아키텍처
+![아키텍처](http://i.imgur.com/haqK19z.png)
+- 준비 사항
+    ```
+    - Nginx
+    - uWSGI
+    - Django
+    - Docker
+    ```
---
+++ docker_hello_django/docker_hello_django/urls.py
@@ -0,0 +1,21 @@
+"""docker_hello_django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from hello_world import views
+
+urlpatterns = [
+    path('', views.hello_wolrd)
+]
