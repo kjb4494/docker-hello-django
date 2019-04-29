337736b7b084854e432e816b2a6544ab82d3c0a4
kjb4494 <rlagnltn1357@gmail.com>
2019-04-29 17:15:22 +0900

    cloud build auto deploy final test

--- docker_hello_django/hello_world/views.py
+++ docker_hello_django/hello_world/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_wolrd(request):
-    return HttpResponse("<title>Test Server</title><h2>Hello world!</h2>This is just Test Server.")
+    return HttpResponse("<title>Test Server</title><h2>Hello world!</h2>This is just Test Server. - 1.6.1 version!")
